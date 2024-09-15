fbd6c5c7ff97dafd8b87cdf626611f5310f9098a
PoemsAndDreams <3513254476@qq.com>
2024-09-15 21:59:54 +0800

    add:项目初始化和完成基本功能

---
+++ src/main/java/com/dreams/question/controller/PostFavourController.java
@@ -0,0 +1,107 @@
+package com.dreams.question.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.dreams.question.common.BaseResponse;
+import com.dreams.question.common.ErrorCode;
+import com.dreams.question.common.ResultUtils;
+import com.dreams.question.exception.BusinessException;
+import com.dreams.question.model.dto.post.PostQueryRequest;
+import com.dreams.question.model.dto.postfavour.PostFavourAddRequest;
+import com.dreams.question.model.dto.postfavour.PostFavourQueryRequest;
+import com.dreams.question.model.entity.Post;
+import com.dreams.question.model.entity.User;
+import com.dreams.question.model.vo.PostVO;
+import com.dreams.question.service.PostFavourService;
+import com.dreams.question.service.PostService;
+import com.dreams.question.service.UserService;
+import com.dreams.question.exception.ThrowUtils;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子收藏接口
+ */
+@RestController
+@RequestMapping("/post_favour")
+@Slf4j
+public class PostFavourController {
+
+    @Resource
+    private PostFavourService postFavourService;
+
+    @Resource
+    private PostService postService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 收藏 / 取消收藏
+     *
+     * @param postFavourAddRequest
+     * @param request
+     * @return resultNum 收藏变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doPostFavour(@RequestBody PostFavourAddRequest postFavourAddRequest,
+                                              HttpServletRequest request) {
+        if (postFavourAddRequest == null || postFavourAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能操作
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postFavourAddRequest.getPostId();
+        int result = postFavourService.doPostFavour(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 获取我收藏的帖子列表
+     *
+     * @param postQueryRequest
+     * @param request
+     */
+    @PostMapping("/my/list/page")
+    public BaseResponse<Page<PostVO>> listMyFavourPostByPage(@RequestBody PostQueryRequest postQueryRequest,
+                                                             HttpServletRequest request) {
+        if (postQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        long current = postQueryRequest.getCurrent();
+        long size = postQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postQueryRequest), loginUser.getId());
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+
+    /**
+     * 获取用户收藏的帖子列表
+     *
+     * @param postFavourQueryRequest
+     * @param request
+     */
+    @PostMapping("/list/page")
+    public BaseResponse<Page<PostVO>> listFavourPostByPage(@RequestBody PostFavourQueryRequest postFavourQueryRequest,
+            HttpServletRequest request) {
+        if (postFavourQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        long current = postFavourQueryRequest.getCurrent();
+        long size = postFavourQueryRequest.getPageSize();
+        Long userId = postFavourQueryRequest.getUserId();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20 || userId == null, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postFavourQueryRequest.getPostQueryRequest()), userId);
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+}
---
+++ src/main/java/com/dreams/question/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.dreams.question.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dreams.question.model.entity.User;
+
+/**
+ * @author xiayutian
+ * @description 针对表【user(用户)】的数据库操作Mapper
+ * @createDate 2024-09-15 14:15:03
+ * @Entity generator.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
